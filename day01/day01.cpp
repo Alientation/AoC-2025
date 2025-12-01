b8a1ad76b1b3d999fa8cec1c2dd25fc59e02d7a2
Bill Ma <billma@utexas.edu>
2025-11-30 23:25:52 -0600

    bruh

--- day01/day01.cpp
+++ day01/day01.cpp
@@ -1,18 +1,87 @@
 #include <iostream>
 
 #include <cstring>
+#include <cassert>
 
 using namespace std;
 
 void p1 ()
 {
-    
+    int p = 50;
+    int count = 0;
+
+    string line;
+    while (getline (cin, line))
+    {
+        char dir = line[0];
+        int amt = stoi (line.substr (1));
+
+        if (dir == 'L')
+        {
+            p -= amt;
+            p += 100 * ((amt + 99) / 100);
+            p %= 100;
+            assert (p >= 0);
+        }
+        else if (dir == 'R')
+        {
+            p += amt;
+            p %= 100;
+        }
+        if (p == 0)
+        {
+            count++;
+        }
+    }
+
+    cout << count;
 }
 
 
 void p2 ()
 {
+    int p = 50;
+    int count = 0;
 
+    string line;
+    while (getline (cin, line))
+    {
+        char dir = line[0];
+        int amt = stoi (line.substr (1));
+
+        if (dir == 'L')
+        {
+            while (amt > 0)
+            {
+                p--;
+                amt--;
+                if (p == 0)
+                {
+                    count++;
+                }
+                else if (p < 0)
+                {
+                    p = 99;
+                }
+            }
+        }
+        else if (dir == 'R')
+        {
+            while (amt > 0)
+            {
+                p++;
+                amt--;
+                if (p == 100)
+                {
+                    count++;
+                    p = 0;
+                }
+            }
+        }
+
+    }
+
+    cout << count;
 }
 
 int main (int argc, char *argv[])
